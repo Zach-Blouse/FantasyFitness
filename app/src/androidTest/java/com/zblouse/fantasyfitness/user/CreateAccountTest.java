05aa972311b189d283f973b79c11285dba213598
Zach Blouse <zachblouse@gmail.com>
2025-02-04 20:28:11 -0500

    checking to see if login succeeds

--- app/src/androidTest/java/com/zblouse/fantasyfitness/user/CreateAccountTest.java
+++ app/src/androidTest/java/com/zblouse/fantasyfitness/user/CreateAccountTest.java
@@ -11,15 +11,33 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.zblouse.fantasyfitness.MainActivity;
 import com.zblouse.fantasyfitness.R;
 
+import org.junit.After;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
 @RunWith(AndroidJUnit4.class)
 public class CreateAccountTest {
+
+    @BeforeClass
+    public static void setupFirebase() {
+        String localHost = "10.0.2.2";
+
+        FirebaseAuth.getInstance().useEmulator(localHost, 9099);
+        FirebaseFirestore.getInstance().useEmulator(localHost, 8080);
+    }
+
+    @After
+    public void tearDownFirebase() {
+        FirebaseAuth.getInstance().signOut();
+    }
 
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
@@ -28,5 +46,8 @@ public class CreateAccountTest {
     public void createAccountTest(){
         onView(withId(R.id.app_title)).check(matches(withText("Fantasy Fitness")));
         onView(withId(R.id.create_account_button)).perform(click());
+        onView(withId(R.id.create_account_title)).check(matches(withText("Create Account")));
     }
+
+
 }
