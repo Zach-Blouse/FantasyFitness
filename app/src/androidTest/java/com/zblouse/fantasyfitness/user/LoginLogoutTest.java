7787fdbf65bad0caf4766cd2fb0058b9d9e243aa
Zach Blouse <zachblouse@gmail.com>
2025-02-06 17:03:16 -0500

    login logout test passing locally

--- app/src/androidTest/java/com/zblouse/fantasyfitness/user/LoginLogoutTest.java
+++ app/src/androidTest/java/com/zblouse/fantasyfitness/user/LoginLogoutTest.java
@@ -3,13 +3,11 @@ package com.zblouse.fantasyfitness.user;
 import static android.app.Activity.RESULT_OK;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.Intents.intending;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
@@ -23,15 +21,12 @@ import android.app.Instrumentation;
 import android.content.Intent;
 import android.os.Looper;
 
-import androidx.test.annotation.UiThreadTest;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
-import com.firebase.ui.auth.KickoffActivity;
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.firebase.ui.auth.data.model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -48,14 +43,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RunWith(AndroidJUnit4.class)
-public class CreateAccountTest {
+public class LoginLogoutTest {
 
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
@@ -65,7 +59,7 @@ public class CreateAccountTest {
             new IntentsTestRule<>(MainActivity.class);
 
     @Test
-    public void createAccountTest(){
+    public void loginLogoutTest(){
 
         // Choose authentication providers
         List<AuthUI.IdpConfig> providers = Arrays.asList(
@@ -100,15 +94,17 @@ public class CreateAccountTest {
         when(mockFirestore.collection(eq("users"))).thenReturn(mockCollectionReference);
         DocumentReference mockDocument = Mockito.mock(DocumentReference.class);
         when(mockCollectionReference.document(eq("testUserId"))).thenReturn(mockDocument);
-        Task<Void> mockWriteTask = Mockito.mock(Task.class);
-        when(mockDocument.set(any())).thenReturn(mockWriteTask);
+
         Task<DocumentSnapshot> mockReadTask = Mockito.mock(Task.class);
         when(mockDocument.get()).thenReturn(mockReadTask);
-        ArgumentCaptor<OnCompleteListener<Void>> onCompleteListenerArgumentCaptorWrite = ArgumentCaptor.forClass(OnCompleteListener.class);
         ArgumentCaptor<OnCompleteListener<DocumentSnapshot>> onCompleteListenerArgumentCaptorRead = ArgumentCaptor.forClass(OnCompleteListener.class);
         DocumentSnapshot mockDocumentSnapshot = Mockito.mock(DocumentSnapshot.class);
 
-        when(mockWriteTask.isSuccessful()).thenReturn(true);
+        when(mockReadTask.isSuccessful()).thenReturn(true);
+        when(mockReadTask.getResult()).thenReturn(mockDocumentSnapshot);
+        when(mockDocumentSnapshot.exists()).thenReturn(true);
+        when(mockDocumentSnapshot.get("UID")).thenReturn("testUid");
+        when(mockDocumentSnapshot.get("USERNAME")).thenReturn("testUsername");
 
         UserFirestoreDatabase userFirestoreDatabase = new UserFirestoreDatabase(mockFirestore);
         UserRepository userRepository = new UserRepository(userService,userFirestoreDatabase);
@@ -118,21 +114,18 @@ public class CreateAccountTest {
         intending(hasComponent("com.firebase.ui.auth.KickoffActivity")).respondWith(firebaseResult);
 
         onView(withId(R.id.app_title)).check(matches(withText("Fantasy Fitness")));
-        onView(withId(R.id.create_account_button)).perform(click());
+        onView(withId(R.id.login_button)).perform(click());
+        Looper.prepare();
         verify(mockReadTask).addOnCompleteListener(onCompleteListenerArgumentCaptorRead.capture());
         onCompleteListenerArgumentCaptorRead.getValue().onComplete(mockReadTask);
-        when(mockReadTask.isSuccessful()).thenReturn(true);
-        when(mockReadTask.getResult()).thenReturn(mockDocumentSnapshot);
-        when(mockDocumentSnapshot.exists()).thenReturn(false);
-
-        onView(withId(R.id.create_account_title)).check(matches(withText("Create Account")));
-        onView(withId(R.id.username_edit_text)).perform(typeText("createAccountTestUsername"));
-        onView(withId(R.id.create_account_button)).perform(click());
-        Looper.prepare();
-        verify(mockWriteTask).addOnCompleteListener(onCompleteListenerArgumentCaptorWrite.capture());
-        onCompleteListenerArgumentCaptorWrite.getValue().onComplete(mockWriteTask);
-
         onView(withId(R.id.user_home_title)).check(matches(withText("Fantasy Fitness")));
+
+        //Logout portion
+        onView(withId(R.id.action_settings)).perform(click());
+        when(mockAuth.getCurrentUser()).thenReturn(null);
+        onView(withId(R.id.logout_button)).perform((click()));
+        verify(mockAuth).signOut();
+        onView(withId(R.id.app_title)).check(matches(withText("Fantasy Fitness")));
 
     }
 
