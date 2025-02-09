a41cba1b0ba08217b8fa39cff46110560f1eb3ea
Zach Blouse <zachblouse@gmail.com>
2025-02-09 17:01:22 -0500

    maybe it is running in europe?

--- app/src/main/java/com/zblouse/fantasyfitness/workout/WorkoutFragment.java
+++ app/src/main/java/com/zblouse/fantasyfitness/workout/WorkoutFragment.java
@@ -148,15 +148,15 @@ public class WorkoutFragment extends AuthenticationRequiredFragment implements E
         remainingMillis = remainingMillis - (minutes*MILLIS_IN_MINUTE);
         long seconds = remainingMillis/MILLIS_IN_SECOND;
         if (hours > 0) {
-            return String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, seconds);
+            return String.format(Locale.US, "%d:%02d:%02d", hours, minutes, seconds);
         } else {
-            return String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+            return String.format(Locale.US, "%02d:%02d", minutes, seconds);
         }
     }
 
     private String formatDistanceDisplay(double distanceMeters){
         double distanceKm = distanceMeters/1000;
-        return String.format(Locale.getDefault(), "%.2f",distanceKm) + " km";
+        return String.format(Locale.US, "%.2f",distanceKm) + " km";
     }
 
     private void initialButtonSetup(){
